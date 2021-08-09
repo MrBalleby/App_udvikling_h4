707bbb631ca85e5ee91fddc9cd29011b78eacd2d
Mark Balleby Hansen <mbal@verdo.com>
2021-08-09 11:00:50 +0200

    Created: HTTPACCESS class WeatherData class WeatherDataSingleton class

---
+++ AnotherWeatherApp/WeatherDataAccess/Model/WeatherDataSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherDataAccess.Model
+{
+    public class WeatherDataSingleton
+    {
+        private static WeatherDataSingleton instance = null;
+        public WeatherDataSingleton() { }
+
+        public static WeatherDataSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new WeatherDataSingleton();
+                }
+                return instance;
+            }
+        }
+
+        public static void Reset()
+        {
+            instance = null;
+        }
+
+        public WeatherData WeatherData;
+
+    }
+}
