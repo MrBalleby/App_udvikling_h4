291a2d629b60198ecdfd488fae31d25d58be08aa
Mark Balleby Hansen <mbal@verdo.com>
2021-08-10 08:59:03 +0200

    Added: Android Project - CurrentWeather

---
+++ AnotherWeatherApp/CurrentWeather/CurrentWeather/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using CurrentWeather.ViewModels;
+using CurrentWeather.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CurrentWeather
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ AnotherWeatherApp/CurrentWeather/CurrentWeather/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CurrentWeather.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CurrentWeather.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
