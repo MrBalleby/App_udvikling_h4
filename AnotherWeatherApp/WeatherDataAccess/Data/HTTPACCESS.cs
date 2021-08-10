eb06892523cfc024c96983626df97ff05a974bdb
Aputsiaq Lindhardt <51225@airgreenland.gl>
2021-08-10 10:28:20 +0200

    httpaccess.cs update

--- AnotherWeatherApp/WeatherDataAccess/Data/HTTPACCESS.cs
+++ AnotherWeatherApp/WeatherDataAccess/Data/HTTPACCESS.cs
@@ -1,15 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WeatherDataAccess.Data
 {
-    public class HTTPACCESS
+    public class HTTPACCESS : IDisposable
     {
         private static string _baseUrl = @"https://api.met.no/weatherapi/locationforecast/2.0/";
+        private HttpClient _httpClient;
+        private bool _disposedValue;
 
-        
+        public HTTPACCESS()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(_baseUrl)
+            };
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
