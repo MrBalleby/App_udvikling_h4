b60e5b30a311fd96647c97ac46174d9757441127
Aputsiaq Lindhardt <51225@airgreenland.gl>
2021-08-12 10:00:42 +0200

    Readded DailyWeatherSummary

---
+++ AnotherWeatherApp/YetAnotherWeatherApp/YetAnotherWeatherApp/Models/DailyWeatherSummary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YetAnotherWeatherApp.Models
+{
+    public class DailyWeatherSummary
+    {
+        public DateTime Date { get; set; }
+        public string IconName { get; set; }
+        public double AirTemperature { get; set; }
+    }
+}
