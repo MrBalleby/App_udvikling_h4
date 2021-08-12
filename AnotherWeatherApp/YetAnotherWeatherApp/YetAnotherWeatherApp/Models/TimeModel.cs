665950cae60730ed9ba58e94d704b4751e62e19c
Aputsiaq Lindhardt <51225@airgreenland.gl>
2021-08-12 12:15:10 +0200

    Updated TimeModel => Made ThisDayConverted get only
    Added commands to HomeViewModel, and bound to them from HomeView.xaml

--- AnotherWeatherApp/YetAnotherWeatherApp/YetAnotherWeatherApp/Models/TimeModel.cs
+++ AnotherWeatherApp/YetAnotherWeatherApp/YetAnotherWeatherApp/Models/TimeModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace YetAnotherWeatherApp.Models
 {
     public class TimeModel
     {
+        CultureInfo _cultureInfo = new CultureInfo("da-DK");
+
         public DateTime Date { get; set; }
-        public string ThisDayConverted { get; set; }
+        public string ThisDayConverted { get => Date.ToString("dd. MMMM yyyy", _cultureInfo); }
     }
 }
--- AnotherWeatherApp/YetAnotherWeatherApp/YetAnotherWeatherApp/ViewModels/HomeViewModel.cs
+++ AnotherWeatherApp/YetAnotherWeatherApp/YetAnotherWeatherApp/ViewModels/HomeViewModel.cs
@@ -3,24 +3,38 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 using YetAnotherWeatherApp.Models;
 
 namespace YetAnotherWeatherApp.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
+        bool dayPickerIsVisible = false;
+        public bool DayPickerIsVisible { 
+            get => dayPickerIsVisible;
+            set 
+            {
+                dayPickerIsVisible = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public ICommand ShowDayPicker { get; set; }
+        public ICommand HideDayPicker { get; set; }
+
         public TimeModel TimeModel { get; set; }
         ObservableCollection<TimeModel> dates = new ObservableCollection<TimeModel>();
         public ObservableCollection<TimeModel> Dates { get { return dates; } }
 
-        CultureInfo CultureInfo = new CultureInfo("da-DK");
         public HomeViewModel()
         {
-
+            ShowDayPicker = new Command(OnShowDayPicker);
+            HideDayPicker = new Command(OnHideDayPicker);
             TimeModel = new TimeModel()
             {
                 Date = DateTime.Now,
-                ThisDayConverted = DateTime.Now.ToString("dd. MMMM yyyy", CultureInfo)
             };
 
             for (int i = 0; i < 6; i++)
@@ -28,9 +42,20 @@ namespace YetAnotherWeatherApp.ViewModels
                 dates.Add(new TimeModel()
                 {
                     Date = DateTime.Now.AddDays(i),
-                    ThisDayConverted = DateTime.Now.ToString("dd. MMMM yyyy", CultureInfo)
                 });
             }
+        }
+
+        void OnShowDayPicker()
+        {
+            if (DayPickerIsVisible is false)
+                DayPickerIsVisible = true;
+        }
+
+        void OnHideDayPicker()
+        {
+            if (DayPickerIsVisible is true)
+                DayPickerIsVisible = false;
         }
     }
 }
