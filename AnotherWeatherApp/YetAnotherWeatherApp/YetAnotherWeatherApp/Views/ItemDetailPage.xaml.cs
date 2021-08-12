9dea000cbba1c55dc476e85506a451e18f458329
Aputsiaq Lindhardt <51225@airgreenland.gl>
2021-08-12 09:00:58 +0200

    Added YetAnotherWeatherApp

---
+++ AnotherWeatherApp/YetAnotherWeatherApp/YetAnotherWeatherApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using YetAnotherWeatherApp.ViewModels;
+
+namespace YetAnotherWeatherApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
