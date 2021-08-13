733cc821f71dd9355bc3dedda9157ef555b9bbc1
Counterlesscount <mbal@verdo.com>
2021-08-13 08:57:09 +0200

    Added DetailedWeather/Settings 130821

---
+++ AnotherWeatherApp/YetAnotherWeatherApp/YetAnotherWeatherApp/ViewModels/DetailedWeatherViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YetAnotherWeatherApp.ViewModels
+{
+    public class DetailedWeatherViewModel : BaseViewModel
+    {
+        public DetailedWeatherViewModel()
+        {
+
+        }
+    }
+}
