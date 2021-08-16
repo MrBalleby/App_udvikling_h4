67dc11c46fc6e4f00b98f1ade1364d32d9bed6cf
Aputsiaq Lindhardt <51225@airgreenland.gl>
2021-08-16 11:04:38 +0200

    Added Data to DailyWeatherSummaryView from WeatherDataAccess

--- AnotherWeatherApp/YetAnotherWeatherApp/YetAnotherWeatherApp/Views/DailyWeatherSummaryView.xaml.cs
+++ AnotherWeatherApp/YetAnotherWeatherApp/YetAnotherWeatherApp/Views/DailyWeatherSummaryView.xaml.cs
@@ -4,7 +4,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using WeatherDataAccess.Data;
+using Xamarin.CommunityToolkit.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using YetAnotherWeatherApp.Models;
@@ -25,18 +27,39 @@ namespace YetAnotherWeatherApp.Views
         };
 
         ObservableCollection<DailyWeatherSummary> dailyWeatherSummaries = new ObservableCollection<DailyWeatherSummary>();
-
+        public AsyncCommand GetDataFromApi { get; set; }
         public DailyWeatherSummaryView()
         {
             InitializeComponent();
 
             DateWeatherSummaryView.ItemsSource = dailyWeatherSummaries;
 
+            GetDataFromApi = new AsyncCommand(OnGetDataFromApi);
+
             dailyWeatherSummaries.Add(new DailyWeatherSummary { Date = DateTime.Today, AirTemperature = GetRandomDouble(5, 40), IconName = RandomWeather() });
             dailyWeatherSummaries.Add(new DailyWeatherSummary { Date = DateTime.Today.AddDays(1), AirTemperature = GetRandomDouble(5, 40), IconName = RandomWeather() });
             dailyWeatherSummaries.Add(new DailyWeatherSummary { Date = DateTime.Today.AddDays(2), AirTemperature = GetRandomDouble(5, 40), IconName = RandomWeather() });
             dailyWeatherSummaries.Add(new DailyWeatherSummary { Date = DateTime.Today.AddDays(3), AirTemperature = GetRandomDouble(5, 40), IconName = RandomWeather() });
             dailyWeatherSummaries.Add(new DailyWeatherSummary { Date = DateTime.Today.AddDays(4), AirTemperature = GetRandomDouble(5, 40), IconName = RandomWeather() });
+        }
+
+        async Task OnGetDataFromApi()
+        {
+            var results = await new HTTPACCESS().GetWeatherFromLocationAsync(56.4520, 9.3963);
+
+            var midDayResults = results.Properties.Timeseries.Where(ts => ts.Time.TimeOfDay == new TimeSpan(12, 0, 0)).ToList();
+            foreach (var item in midDayResults)
+            {
+                dailyWeatherSummaries.Add(new DailyWeatherSummary { AirTemperature = item.Data.Instant.Details.AirTemperature, Date = item.Time, IconName = item.Data.NextSixHours.Summary.SymbolCode });
+            }
+        }
+
+        void SetDailyWeatherFromTimeSeries(List<WeatherDataAccess.Model.TimeData> timeDatas)
+        {
+            foreach (var item in timeDatas)
+            {
+                dailyWeatherSummaries.Add(new DailyWeatherSummary { AirTemperature = item.Data.Instant.Details.AirTemperature, Date = item.Time, IconName = item.Data.NextHour.Summary.SymbolCode });
+            }
         }
 
         double GetRandomDouble(double min, double max)
@@ -51,5 +74,15 @@ namespace YetAnotherWeatherApp.Views
         }
 
         public ObservableCollection<DailyWeatherSummary> DailyWeatherSummaries { get; set; }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await OnGetDataFromApi();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            dailyWeatherSummaries.Clear();
+        }
     }
 }
